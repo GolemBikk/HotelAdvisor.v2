8d3164c2d37584e31ee3a6c36729c055239bd23e
Alexander Iundin <dorian.adret@gmail.com>
2017-06-29 14:13:22 +0300

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = 'Hotel Advisor v2'
+    return base_title if page_title.empty?
+    return page_title
+  end
 end
